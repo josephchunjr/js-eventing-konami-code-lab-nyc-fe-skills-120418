1b4807612fa863747213131274e3212fb28ef722
joseph Chun <jjunior.chun@gmail.com>
2019-01-31 23:04:11 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,26 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+let tracker = 0
+
 
 function init() {
   // Write your JavaScript code inside the init() function
 
+// 2 make the function to trigger
+const checkKeyCode = () => {
+  const whatTheyPressed = event.keyCode 
+  if (whatTheyPressed === code[tracker]) {
+    tracker += 1
+  } else {
+    tracker = 0
+  }
+  console.log(tracker)
+  
+}
+
+
+
+}
+
+function inti () {
+  document.addEventListener("keydown", checkKeyCode)
 }
